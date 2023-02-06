a5abad47a2fa8c9bb2af7fd717ca90bacd5acf36
muhsin-life <123951517+muhsin-life@users.noreply.github.com>
2023-02-06 08:01:59 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ fetch(`https://prodapp.lifepharmacy.com/api/categories`)
   .then(data => {
     data.data.map(cat_data => {
 
-      cat_element.innerHTML += '<li> <button href="#" class=" w-full py-3 pr-10 pl-5 text-left flex hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white;"> <span class=" flex-1">' + cat_data.name + '</span> <span class="mr-auto my-auto"> <svg class="fill-current h-4 w-4 transition duration-150 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"> <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg> </span> </button> ' +
+      cat_element.innerHTML += '<li> <button href="#" class=" w-full py-3 pr-10 pl-5 text-left flex  border-gray-200 border-b-2 hover:text-blue-500"> <span class=" flex-1">' + cat_data.name + '</span> <span class="mr-auto my-auto"> <svg class="fill-current h-4 w-4 transition duration-150 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"> <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg> </span> </button> ' +
         '<ul class="bg-white border rounded-sm absolute top-0 right-0 transition duration-200 ease-in-out origin-top-left hover-menu p-5 h-[32rem] overflow-auto border border-gray-300"> ' +
-        '<li class="px-3 py-1 hover:bg-gray-100 ">' +
+        '<li class="px-3 py-1 ">' +
         '<div class="grid grid-cols-4 gap-x-10 gap-y-3 grid-rows-6 cat-elements mb-10">' +
         categoryChildrenData(cat_data.children) +
         '</div>  </li> </ul> </li>'
@@ -36,10 +36,10 @@ function sectionsData(sectionsData, sectionName) {
   
   for (var i = 0; i < sectionsData.length; i++) {
     if (i < limit) {
-      data_section += '<div class="mb-3">' + sectionsData[i].name + '</div>'
+      data_section += '<div class="hover:text-blue-500 mb-3"><a href="#">' + sectionsData[i].name + '</a></div>'
     }
     else {        
-      data_section += '<div class="mb-3 hidden ' + sect_name + '">' + sectionsData[i].name + '</div>'
+      data_section += '<div class="hover:text-blue-500 mb-3 hidden ' + sect_name + '"><a href="#">' + sectionsData[i].name + '</a></div>'
     }
   }
   // for(var i of sectionsData){
@@ -58,9 +58,9 @@ fetch(`https://prodapp.lifepharmacy.com/api/web/brands`)
 
     data.data.brands.map(brand_data => {
 
-      brands_section.innerHTML += '<div class="grid-flow-row mb-10"> <div class="flex flex-col mr-5">' +
-        '<img class="mx-auto" src="' + brand_data.images.logo + '" alt="" width="150px">' +
-        '<h5 class="text-center">' + brand_data.name + '</h5>' +
+      brands_section.innerHTML += '<div class="grid-flow-row mb-5"> <div class="flex flex-col mr-5">' +
+        '<img class="mx-auto rounded-full border border-white bg-white shadow-md" src="' + brand_data.images.logo + '" alt="" width="150px">' +
+        '<h5 class="text-center mt-3">' + brand_data.name + '</h5>' +
         '</div></div>'
 
     });
